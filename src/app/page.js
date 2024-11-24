e2c23d193e49c711a26b5a73e014202c6866063e
deveshbis <deveshbiswas.cse@gmail.com>
2024-11-24 14:27:00 +0600

    added footer section

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,8 @@
 import Header from "./componets/Header/Header";
 import Banner from "./componets/Banner/Banner";
 import BannerStats from "./componets/BannerStats/BannerStats";
+import FeaturedProduct from "./componets/FeaturedProduct/FeaturedProduct";
+import Footer from "./componets/Footer/Footer";
 
 export default function Home() {
   return (
@@ -9,6 +11,8 @@ export default function Home() {
       <Header />
       <Banner />
       <BannerStats />
+      {/* <FeaturedProduct/> */}
+      <Footer/>
     </>
 
   );
