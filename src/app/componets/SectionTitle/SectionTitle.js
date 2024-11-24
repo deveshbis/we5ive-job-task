7aa927f0706d88ea1a9f5d476e916bff33e48e4d
deveshbis <deveshbiswas.cse@gmail.com>
2024-11-24 12:16:04 +0600

    created section title component

---
+++ src/app/componets/SectionTitle/SectionTitle.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const SectionTitle = ({subHeader,mainHeader}) => {
+    return (
+        <div>
+            <h3 className="uppercase text-gray-500 font-semibold leading-4">
+                {subHeader}
+            </h3>
+            <h2 className="text-primary font-bold text-4xl italic">
+                {mainHeader}
+            </h2>
+        </div>
+    )
+}
+
+export default SectionTitle
