a99dc9a2f2e1f9d4fda9a81ffdb748900af5d415
deveshbis <deveshbiswas.cse@gmail.com>
2024-11-24 10:59:39 +0600

    added banner option stats

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,14 @@
 
 import Header from "./componets/Header/Header";
 import Banner from "./componets/Banner/Banner";
+import BannerStats from "./componets/BannerStats/BannerStats";
 
 export default function Home() {
   return (
     <>
       <Header />
       <Banner />
+      <BannerStats />
     </>
 
   );
